dd53ab95c030437b524b654c7e12aebd09ef2965
Joshua Beh <joshbeh88@gmail.com>
2023-01-10 17:42:21 +0000

    Made recommended changes and added a 0 to the scores of the anime with a length of 3. Also moved the down arrow padding to a div.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,50 @@ var $arrowDown = document.querySelector('.fa-arrow-down');
 var $backToTop = document.querySelector('.back-to-top');
 var $topOfPage = document.querySelector('.anchor-background');
 var pageNumber = 1;
+
+function renderTopAnime(response, i) {
+  var li = document.createElement('li');
+
+  var rowDiv = document.createElement('div');
+  rowDiv.className = 'row top-background';
+  li.appendChild(rowDiv);
+
+  var col20Div = document.createElement('div');
+  col20Div.className = 'col-20 center';
+  rowDiv.appendChild(col20Div);
+
+  var img = document.createElement('img');
+  img.setAttribute('src', response[i].images.jpg.small_image_url);
+  img.className = 'top-image';
+  col20Div.appendChild(img);
+
+  var col80Div = document.createElement('div');
+  col80Div.className = 'col-80 inline';
+  rowDiv.appendChild(col80Div);
+
+  var title = document.createElement('a');
+  title.setAttribute('href', '#');
+  title.className = 'top-titles';
+  title.textContent = response[i].title;
+  col80Div.appendChild(title);
+
+  var h1Score = document.createElement('h1');
+  h1Score.className = 'top-scores inline star-hide';
+  if (response[i].score.toString().length === 3) {
+    h1Score.textContent = response[i].score + '0';
+  } else {
+    h1Score.textContent = response[i].score;
+  }
+  col80Div.appendChild(h1Score);
+
+  var iStar = document.createElement('i');
+  iStar.className = 'fa-solid fa-star';
+  h1Score.prepend(iStar);
+  $topAnimeList.appendChild(li);
+
+  return $topAnimeList;
+}
+
 function topAnimeGet() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.jikan.moe/v4/top/anime?page=' + pageNumber);
@@ -10,42 +54,8 @@ function topAnimeGet() {
   xhr.addEventListener('load', function () {
     var response = xhr.response.data;
     for (var i = 0; i < response.length; i++) {
-      var li = document.createElement('li');
-
-      var rowDiv = document.createElement('div');
-      rowDiv.className = 'row top-background';
-      li.appendChild(rowDiv);
-
-      var col20Div = document.createElement('div');
-      col20Div.className = 'col-20 center';
-      rowDiv.appendChild(col20Div);
-
-      var img = document.createElement('img');
-      img.setAttribute('src', response[i].images.jpg.small_image_url);
-      img.className = 'top-image';
-      col20Div.appendChild(img);
-
-      var col80Div = document.createElement('div');
-      col80Div.className = 'col-80 inline';
-      rowDiv.appendChild(col80Div);
-
-      var title = document.createElement('a');
-      title.setAttribute('href', '#');
-      title.className = 'top-titles';
-      title.textContent = response[i].title;
-      col80Div.appendChild(title);
-
-      var h1Score = document.createElement('h1');
-      h1Score.className = 'top-scores inline star-hide';
-      h1Score.textContent = response[i].score;
-      col80Div.appendChild(h1Score);
-
-      var iStar = document.createElement('i');
-      iStar.className = 'fa-solid fa-star';
-      h1Score.prepend(iStar);
-      $topAnimeList.appendChild(li);
+      renderTopAnime(response, i);
     }
-    return $topAnimeList;
   });
   xhr.send();
 }
